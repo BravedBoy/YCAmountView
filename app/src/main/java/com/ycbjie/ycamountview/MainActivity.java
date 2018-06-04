8b7e48779f388157ab5b1ed12caf0e71de1c9072
yangchong211 <yangchong211@163.com>
2018-06-04 09:32:07 +0800

    YCAmountView

---
+++ app/src/main/java/com/ycbjie/ycamountview/MainActivity.java
@@ -0,0 +1,22 @@
+package com.ycbjie.ycamountview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ycbjie.amountviewlib.AmountView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        AmountView avView = findViewById(R.id.av_view);
+        avView.setAmountNum(2,10,1);
+        int amount = avView.getAmount();
+    }
+}
